c87206b4c572b263d91d33036a2011d494ac7224
CoderGirl <angi.adema@gmail.com>
2023-10-09 17:53:56 -0600

    added loading code to App.jsx in order to log "Loading" while the fetch data loads - still working on this

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,10 @@ import { Users } from "./Users";
 export default function App() {
   const [user, setUser] = useState([]);
   const [error, setError] = useState();
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
+    setLoading(true);
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((res) => {
         if (res.status === 200) {
@@ -17,8 +19,19 @@ export default function App() {
       .then((data) => {
         setUser(data);
       })
-      .catch((err) => setError(err));
+      .catch((err) => setError(err))
+      .finally(() => setLoading(false));
   }, []);
+
+  let jsx;
+
+  if (loading) {
+    jsx = <h2>Loading...</h2>;
+  } else if (error != null) {
+    jsx = <h2>Error Loading Data!</h2>;
+  } else {
+    jsx = JSON.stringify(user);
+  }
 
   return (
     <>
