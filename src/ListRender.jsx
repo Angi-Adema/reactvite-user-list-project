4213b1e30791c0f43aab0bf1bb642a2cda1a405b
CoderGirl <angi.adema@gmail.com>
2023-10-09 14:27:24 -0600

    added ListRender.jsx component and code

---
+++ src/ListRender.jsx
@@ -0,0 +1,44 @@
+import { useEffect, useState } from "react";
+
+function ListRender() {
+  const [list, setList] = useState();
+  const [error, setError] = useState();
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => {
+        if (res.status === 200) {
+          return res.json();
+        } else {
+          return Promise.reject(res);
+        }
+      })
+      .then((data) => {
+        setList(data);
+      })
+      .catch((err) => setError(err))
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []);
+
+  let jsx;
+
+  if (loading) {
+    jsx = <h1>Loading...</h1>;
+  } else if (error != null) {
+    jsx = <h1>Error loading data!</h1>;
+  } else {
+    jsx = JSON.stringify(list);
+  }
+
+  return (
+    <div>
+      <h1>User List</h1>
+      {jsx}
+    </div>
+  );
+}
