57c96eb9dea20b473a27bded609095e8e2dfe20f
CoderGirl <angi.adema@gmail.com>
2023-10-09 16:58:00 -0600

    deleted ListRender.jsx and replaced with Users.jsx and code to render list names, updated App.jsx with the code that was in ListRender.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,49 @@
-function App() {
-  return <ListRender />;
-}
+import { useEffect, useState, Fragment } from "react";
+import { Users } from "./Users";
 
-export default App;
+export default function App() {
+  const [list, setList] = useState({ jsx });
+  const [error, setError] = useState();
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => {
+        if (res.status === 200) {
+          return res.json();
+        } else {
+          return Promise.reject(res);
+        }
+      })
+      .then((data) => {
+        setList(data);
+      })
+      .catch((err) => setError(err))
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []);
+
+  let jsx;
+
+  if (loading) {
+    jsx = <h1>Loading...</h1>;
+  } else if (error != null) {
+    jsx = <h1>Error loading data!</h1>;
+  } else {
+    jsx = JSON.stringify(list);
+  }
+
+  return (
+    <>
+      <h1>User List</h1>
+      <ul>
+        {list.map((item) => {
+          return <Fragment key={item.id}>{item.name}</Fragment>;
+        })}
+      </ul>
+    </>
+  );
+}
